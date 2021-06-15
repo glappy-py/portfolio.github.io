f9cfa19a212e64e7e3e9807024a909dac0be91d1
GLAPPY <satwikv7@gmail.com>
2021-06-15 13:25:21 +0530

    added backend to count users

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const express = require("express");
+const { count } = require("console");
+const app = express();
+const PORT = 8080;
+app.use(express.static(__dirname));
+
+const increaseCount = () => {
+  let count = Number.parseInt(fs.readFileSync("counter.txt")) + 1;
+  console.log(count);
+  fs.writeFileSync("counter.txt", count.toString());
+};
+
+// PATHS
+app.get("/", (req, res) => {
+  increaseCount();
+  res.sendFile(__dirname + "/home.html");
+});
+
+app.get("/about", (req, res) => {
+  increaseCount();
+  res.sendFile(__dirname + "/about.html");
+});
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/contact.html");
+});
+app.get("/projects", (req, res) => {
+  res.sendFile(__dirname + "/projects.html");
+});
+// counter endpoint
+app.get("/getcount", (req, res) => {
+  let count = fs.readFileSync("counter.txt");
+  res.send(`<h1>${count}</h1>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
+});
